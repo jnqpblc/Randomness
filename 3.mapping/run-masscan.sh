21689d6ddec3ff4d6fe78ee6fbf53e9feaf35ace
John Q. Public <jnqpblc@gmail.com>
2015-07-10 22:04:39 -0500

    Create run-masscan.sh

---
+++ 3.mapping/run-masscan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -z $3 ]; then printf "\nSytnax: $0 <client> <file of ips> <rate|e.g. 1000>\n\n"
+	else
+   CLIENT=$1; FILE=$2; RATE=$3;
+   echo "~/masscan/bin/masscan -iL $FILE -p T:0-65535 --rate $RATE --banners -oB ~/$CLIENT/masscan-output.bin --interface eth0"
+   sudo ~/masscan/bin/masscan -iL $FILE -p T:0-65535 --rate $RATE --banners -oB ~/$CLIENT/masscan-output.bin --interface eth0|tee ~/$CLIENT/masscan-output.txt
+   check=$(ls ~/$CLIENT/masscan-output.bin > /dev/null 2>&1); if [ $? -eq 0 ];then
+      ~/masscan/bin/masscan --open --banners --readscan ~/$CLIENT/masscan-output.bin -oX ~/$CLIENT/masscan-output.xml
+      ~/masscan/bin/masscan --readscan ~/$CLIENT/masscan-output.bin > ~/$CLIENT/masscan-output.txt
+   fi
+fi
