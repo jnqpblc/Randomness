3812b642c5bd6c21ad1eb26f90f0cc5fac1371f1
John Q. Public <jnqpblc@gmail.com>
2015-07-10 23:03:25 -0500

    Create run_nikto_automate.sh

---
+++ 3.mapping/run_nikto_automate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ -z $3 ];then printf "\nSyntax: $0 <client> <proto> <file with tagets>\n\n"
+	else
+CLIENT=$1; PROTO=$2; FILE=$3;
+for TARGET in $(cat "$FILE"); do
+	nikto -host $PROTO://TARGET | tee "$CLIENT/nikto_automate_$TARGET_$PROTO.txt"
+done
+fi
