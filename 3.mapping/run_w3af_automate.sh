1c050f7c5751eb23033b92cbde316ee4b9298216
John Q. Public <jnqpblc@gmail.com>
2015-07-22 15:28:40 -0500

    Update run_w3af_automate.sh

--- 3.mapping/run_w3af_automate.sh
+++ 3.mapping/run_w3af_automate.sh
@@ -5,16 +5,16 @@ if [ -z $3 ];then printf "\nSyntax: $0  <client> <proto> <file with tagets> <os>
 			w3af-it;
 			exit 0;
 		else
-			OS=$4; LANG=$5;		
+			OS=$4; LANG=$5;	
 			w3af-it;
 			exit 0;
 		exit 0;
 		fi
 
-	w3af-it(); {
+	w3af-it() {
 	CLIENT=$1; PROTO=$2; FILE=$3;
 	for TARGET in $(cat "$FILE"); do
-		cat w3af_automate_template.w3af|sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/$PROTO/g; s/{TARGET}/$TARGET/g; s/{OS}/$OS/g; s/{LANG}/$LANG/g;" > "~/$CLIENT/w3af_automate_$TARGET.w3af";;
+		cat w3af_automate_template.w3af|sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/$PROTO/g; s/{TARGET}/$TARGET/g; s/{OS}/$OS/g; s/{LANG}/$LANG/g;" > "~/$CLIENT/w3af_automate_$TARGET.w3af";
 		w3af_console -s "~/$CLIENT/w3af_automate_$TARGET.w3af";
 	done
 	}
