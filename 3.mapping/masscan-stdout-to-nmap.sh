18e699c46f5ba3e19b786e3751b814d86c1dd899
John Q. Public <jnqpblc@gmail.com>
2015-07-22 15:00:31 -0500

    Update masscan-stdout-to-nmap.sh

--- 3.mapping/masscan-stdout-to-nmap.sh
+++ 3.mapping/masscan-stdout-to-nmap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ -z $2 ]; then printf "\nSytnax: $0 <massscan.out> <client>\n\n"
 	else
-FILE=$1; CLIENT=$2; for host in $(awk '{print $6}' $FILE|sort -u); do
+FILE=$1; CLIENT=$2; for HOST in $(awk '{print $6}' $FILE|sort -u); do
    PORTS=$(grep "$HOST" $FILE|awk '{print $4}'|cut -d'/' -f1|tr '\n' ','|sed 's/,$//g';);
-   echo "sudo nmap -PN -sSV -p $PORTS --script 'ALL and not (broadcast or dos or fuzzer)' --open -oA ~/$CLIENT/$HOST $HOST"
-   sudo nmap -PN -sSV -p $PORTS --script 'ALL and not (broadcast or dos or fuzzer)' --open -oA ~/$CLIENT/$HOST $HOST
+   # $uag & $apikey are from "Randomness/0.setup/.bashrc"
+   echo "sudo nmap -PN -sSV -p $PORTS --script 'ALL and not (broadcast or dos or fuzzer)' $uag $apikey --open -oA ~/$CLIENT/$HOST $HOST"
+   sudo nmap -PN -sSV -p $PORTS --script 'ALL and not (broadcast or dos or fuzzer)' $uag $apikey --open -oA ~/$CLIENT/$HOST $HOST
    done
 fi
