f85d58a95d786c1f1e9e32e8c89fa8cf1adedb8b
John Q. Public <jnqpblc@gmail.com>
2015-07-22 15:49:50 -0500

    Update run_w3af_automate.sh

--- 3.mapping/run_w3af_automate.sh
+++ 3.mapping/run_w3af_automate.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-if [ -z $3 ];then printf "\nSyntax: $0  <client> <proto> <file with tagets> <os> <web language>\n\n"
+if [ -z $3 ];then printf "\nSyntax: $0 <client> <proto> <file with tagets> <os> <web language>\n\n"
 	else
-		if [ -z $4 ];then OS="unknown"; LANG="unknown";
+		w3af-it() {
+			for TARGET in $(cat "$FILE"); do
+				cat "$(echo $0|sed 's/run_w3af_automate.sh//g')w3af_automate_template.w3af"|sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/$PROTO/g; s/{TARGET}/$TARGET/g; s/{OS}/$OS/g; s/{LANG}/$LANG/g;" > ~/$CLIENT/w3af-automate-$TARGET-$PROTO.w3af;
+				w3af_console -s ~/$CLIENT/w3af-automate-$TARGET-$PROTO.w3af;
+			done
+		}
+		if [ -z $4 ];then CLIENT="$1"; PROTO="$2"; FILE="$3"; OS="unknown"; LANG="unknown";
 			w3af-it;
 			exit 0;
 		else
-			OS=$4; LANG=$5;	
+			CLIENT="$1"; PROTO="$2"; FILE="$3"; OS="$4"; LANG="$5";
 			w3af-it;
 			exit 0;
 		exit 0;
 		fi
-
-	w3af-it() {
-	CLIENT=$1; PROTO=$2; FILE=$3;
-	for TARGET in $(cat "$FILE"); do
-		cat w3af_automate_template.w3af|sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/$PROTO/g; s/{TARGET}/$TARGET/g; s/{OS}/$OS/g; s/{LANG}/$LANG/g;" > "~/$CLIENT/w3af_automate_$TARGET.w3af";
-		w3af_console -s "~/$CLIENT/w3af_automate_$TARGET.w3af";
-	done
-	}
 fi
