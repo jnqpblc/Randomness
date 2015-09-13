1f201252203cdf0902b472792deb5a8cabf8b052
John Q. Public <jnqpblc@gmail.com>
2015-09-12 21:13:25 -0500

    Create nikto-txt-to-sqlmap.sh

---
+++ 3.mapping/nikto-txt-to-sqlmap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# git clone https://github.com/sqlmapproject/sqlmap.git
+if [ -z $1 ]; then printf "\nSytnax: $0 <client>\n\n"
+	else
+CLIENT=$1; cd ~/sqlmap/; for TARGET in $(grep -l 'Target IP' ~/$CLIENT/nikto-output-*.txt|sort -u|awk -F- '{print $3":"$4":"$5}'); do
+   HOST=$(echo $TARGET|cut -d':' -f1); PORT=$(echo $TARGET|cut -d':' -f2); PROTO=$(echo $TARGET|cut -d':' -f3);
+   if [ "$PROTO" == "ssl.txt" ];then
+      python sqlmap.py --forms --batch --crawl=7 --level=3 --risk=1 -u https://$HOST:$PORT/ | tee ~/$CLIENT/sqlmap-output-$HOST-$PORT-https.txt
+   elif [ "$PROTO" == "nossl.txt" ];then
+      python sqlmap.py --forms --batch --crawl=7 --level=3 --risk=1 -u http://$HOST:$PORT/ | tee ~/$CLIENT/sqlmap-output-$HOST-$PORT-http.txt
+   else
+	echo "Dammit Bobby. $TARGET is a failure."
+   fi
+   done
+fi
