ff3aa3925475d30f5de4e0c31e6bbefe49690329
John Q. Public <jnqpblc@gmail.com>
2015-10-26 16:06:46 -0500

    Create arin-recon.sh

---
+++ 1.recon/arin-recon.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ -z $1 ];then printf "\nSyntax: $0 <'query'|'example*'|'example*limited*'|'*example*'>\n\n"
+	else
+query=$1
+
+for organization in $(curl "http://whois.arin.net/rest/orgs;name=$query" 2>/dev/null | egrep -o 'C[0-9]{8}' | sort -u); do
+	for netblock in $(curl "http://whois.arin.net/rest/org/$customer/nets" 2>/dev/null | egrep -o 'NET-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]' | sort -u); do
+		echo "### $organization - $netblock:"
+		curl "http://whois.arin.net/rest/net/$netblock.txt" 2>/dev/null | egrep 'NetRange|CIDR|NetName|NetHandle|Customer|RegDate';
+		echo ""
+	done
+done
+
+for customer in $(curl "http://whois.arin.net/rest/customers;name=$query" 2>/dev/null | egrep -o 'C[0-9]{8}' | sort -u); do
+	for netblock in $(curl "http://whois.arin.net/rest/customer/$customer/nets" 2>/dev/null | egrep -o 'NET-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]' | sort -u); do
+		echo "### $customer - $netblock:"
+		curl "http://whois.arin.net/rest/net/$netblock.txt" 2>/dev/null | egrep 'NetRange|CIDR|NetName|NetHandle|Customer|RegDate';
+		echo ""
+	done
+done
+fi
