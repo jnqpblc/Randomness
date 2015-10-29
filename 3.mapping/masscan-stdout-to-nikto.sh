e3a986a2ddb921f95ac9da6b963dc542246d2198
John Q. Public <jnqpblc@gmail.com>
2015-10-28 20:47:32 -0500

    Update masscan-stdout-to-nikto.sh

--- 3.mapping/masscan-stdout-to-nikto.sh
+++ 3.mapping/masscan-stdout-to-nikto.sh
@@ -2,7 +2,7 @@
 # git clone https://github.com/sullo/nikto.git
 if [ -z $2 ]; then printf "\nSytnax: $0 <massscan.out> <client>\n\n"
 	else
-FILE=$1; CLIENT=$2; for host in $(awk '{print $6}' $FILE|sort -u); do
+FILE=$1; CLIENT=$2; for HOST in $(awk '{print $6}' $FILE|sort -u); do
    for PORT in $(grep "$HOST" $FILE|awk '{print $4}'|cut -d'/' -f1;); do
       echo "perl ~/nikto/program/nikto.pl -nossl -host http://$HOST:$PORT/ -F htm -output ~/$CLIENT/nikto-output-$HOST-$PORT-nossl.html|tee  ~/$CLIENT/nikto-output-$HOST-$PORT-nossl.txt"
       perl ~/nikto/program/nikto.pl -nossl -host http://$HOST:$PORT/ -F htm -output ~/$CLIENT/nikto-output-$HOST-$PORT-nossl.html|tee  ~/$CLIENT/nikto-output-$HOST-$PORT-nossl.txt
