73503affcacfed6a2b26846f458c05dc674ab46f
John Q. Public <jnqpblc@gmail.com>
2016-01-15 21:47:57 -0600

    Fixed line 6
    Replaced [egrep -o 'C[0-9]{8}'] with [egrep -o 'handle="[^"]*"' | awk -F'"' '{print $2}']

--- 1.recon/arin-recon.sh
+++ 1.recon/arin-recon.sh
@@ -3,7 +3,7 @@ if [ -z $1 ];then printf "\nSyntax: $0 <'query'|'example*'|'example*limited*'|'*
 	else
 query=$1
 
-for organization in $(curl "http://whois.arin.net/rest/orgs;name=$query" 2>/dev/null | egrep -o 'C[0-9]{8}' | sort -u); do
+for organization in $(curl "http://whois.arin.net/rest/orgs;name=$query" 2>/dev/null | egrep -o 'handle="[^"]*"' | awk -F'"' '{print $2}' | sort -u); do
 	for netblock in $(curl "http://whois.arin.net/rest/org/$customer/nets" 2>/dev/null | egrep -o 'NET-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]{1,3}-[0-9]' | sort -u); do
 		echo "### $organization - $netblock:"
 		curl "http://whois.arin.net/rest/net/$netblock.txt" 2>/dev/null | egrep 'NetRange|CIDR|NetName|NetHandle|Customer|RegDate';
