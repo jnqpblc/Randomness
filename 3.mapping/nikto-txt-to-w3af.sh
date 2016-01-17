c6d44bca40e25d1341bbbaf2886dd809320a966e
John Q. Public <jnqpblc@gmail.com>
2016-01-17 13:50:35 -0600

    Update nikto-txt-to-w3af.sh

--- 3.mapping/nikto-txt-to-w3af.sh
+++ 3.mapping/nikto-txt-to-w3af.sh
@@ -2,14 +2,14 @@
 # git clone https://github.com/andresriancho/w3af.git
 if [ -z $1 ]; then printf "\nSytnax: $0 <client> <profile.w3af>\n\n"
 	else
-CLIENT=$1; cd ~/w3af/; for TARGET in $(grep -l 'Target IP' ~/$CLIENT/nikto-output-*.txt|sort -u|awk -F- '{print $3":"$4":"$5}'); do
+CLIENT=$1; PROFILE=$2; cd ~/w3af/; for TARGET in $(grep -l 'Target IP' ~/$CLIENT/nikto-output-*.txt|sort -u|awk -F- '{print $3":"$4":"$5}'); do
    HOST=$(echo $TARGET|cut -d':' -f1); PORT=$(echo $TARGET|cut -d':' -f2); PROTO=$(echo $TARGET|cut -d':' -f3);
    if [ "$PROTO" == "ssl.txt" ];then
-      sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/https/g; s/{TARGET}/$HOST/g; s/{PORT}/$PORT/g;" ../$PROFILE > ~/$CLIENT/w3af-input-$HOST-$PORT-https.w3af
-      sudo w3af_console -s ~/$CLIENT/w3af-input-$HOST-$PORT-https.w3af
+      sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/https/g; s/{TARGET}/$HOST/g; s/{PORT}/$PORT/g;" ~/$PROFILE > ~/$CLIENT/w3af-input-$HOST-$PORT-https.w3af
+      w3af_console -s ~/$CLIENT/w3af-input-$HOST-$PORT-https.w3af
    elif [ "$PROTO" == "nossl.txt" ];then
-      sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/http/g; s/{TARGET}/$HOST/g; s/{PORT}/$PORT/g;" ../$PROFILE > ~/$CLIENT/w3af-input-$HOST-$PORT-http.w3af
-      sudo w3af_console -s ~/$CLIENT/w3af-input-$HOST-$PORT-http.w3af
+      sed "s/{CLIENT}/$CLIENT/g; s/{PROTO}/http/g; s/{TARGET}/$HOST/g; s/{PORT}/$PORT/g;" ~/$PROFILE > ~/$CLIENT/w3af-input-$HOST-$PORT-http.w3af
+      w3af_console -s ~/$CLIENT/w3af-input-$HOST-$PORT-http.w3af
    else
 	echo "Dammit Bobby. $TARGET is a failure."
    fi
