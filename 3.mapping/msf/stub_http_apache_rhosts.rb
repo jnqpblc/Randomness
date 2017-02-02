07383174b48bb98af815def43714a2f92d70ab41
John Q. Public <jnqpblc@gmail.com>
2017-02-01 20:30:40 -0600

    Create stub_http_apache_rhosts.rb

---
+++ 3.mapping/msf/stub_http_apache_rhosts.rb
@@ -0,0 +1,27 @@
+set SSL false
+
+<ruby>
+framework.db.hosts.each do |host|
+  host.services.each do |service|
+    if service.info =~ /apache/i && service.name == "http" || service.info =~ /apache/i && service.name == "www"
+      self.run_single("set RHOSTS #{host.address}")
+      self.run_single("set RPORT #{service.port}")
+      self.run_single("run")
+    end
+  end
+end
+</ruby>
+
+set SSL true
+
+<ruby>
+framework.db.hosts.each do |host|
+  host.services.each do |service|
+    if service.info =~ /apache/i && service.name == "https" || service.info =~ /apache/i && service.name == "ssl/http"
+      self.run_single("set RHOSTS #{host.address}")
+      self.run_single("set RPORT #{service.port}")
+      self.run_single("run")
+    end
+  end
+end
+</ruby>
