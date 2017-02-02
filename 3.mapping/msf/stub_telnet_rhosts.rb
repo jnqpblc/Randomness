747accd94aded7a8026f86f150f186561b8c1608
John Q. Public <jnqpblc@gmail.com>
2017-02-01 20:46:53 -0600

    Create stub_telnet_rhosts.rb

---
+++ 3.mapping/msf/stub_telnet_rhosts.rb
@@ -0,0 +1,11 @@
+<ruby>
+framework.db.hosts.each do |host|
+  host.services.each do |service|
+    if service.name == "telnet"
+      self.run_single("set RHOSTS #{host.address}")
+      self.run_single("set RPORT #{service.port}")
+      self.run_single("run")
+    end
+  end
+end
+</ruby>
