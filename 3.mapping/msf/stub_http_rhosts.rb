fbacc50ebf245ff792e56b4fa295640acc7b96ae
John Q. Public <jnqpblc@gmail.com>
2017-02-01 20:27:12 -0600

    Create stub_http_rhosts.rb

---
+++ 3.mapping/msf/stub_http_rhosts.rb
@@ -0,0 +1,27 @@
+set SSL false
+
+<ruby>
+framework.db.hosts.each do |host|
+  host.services.each do |service|
+    if service.name == "http" || service.name == "www"
+      self.run_single("set RHOSTS #{host.address}")
+      self.run_single("set RPORT #{service.port}")
+      self.run_single("run")
+    end
+  end
+end
+</ruby>
+
+set SSL true
+
+<ruby>
+framework.db.hosts.each do |host|
+  host.services.each do |service|
+    if service.name == "https" || service.name == "ssl/http"
+      self.run_single("set RHOSTS #{host.address}")
+      self.run_single("set RPORT #{service.port}")
+      self.run_single("run")
+    end
+  end
+end
+</ruby>
