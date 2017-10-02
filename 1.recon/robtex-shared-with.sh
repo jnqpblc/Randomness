0b68fb447364d147074b1cf4bb256105784439e3
jnqpblc <jnqpblc@gmail.com>
2017-10-02 16:44:44 -0500

    Pushing changes...

---
+++ 1.recon/robtex-shared-with.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z $1 ];then printf "\nSyntax: $0 <ip|23.239.11.30>\n\n"
+echo "To automate:
+$ bash arin-recon.sh '*example*'|grep CIDR|awk '{print $2}'|sort -u > /tmp/targets.txt
+$ for CIDR in \`< /tmp/targets.txt\`; do for IP in \`python ../subnet2ip.py $CIDR\`; do bash robtex-also-pointing-too.sh $IP; sleep .1; done;done
+"
+
+	else
+IP=$1
+RES=`curl -A 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36' -s https://www.robtex.com/ip-lookup/$IP | grep "Using as IP number"`
+if [ $? == 0 ];then
+	for LINE in `echo $RES|egrep -o 'dns-lookup\/[^"]*'|cut -d'/' -f2|sort -u`; do
+		echo "$IP - $LINE";
+	done
+fi
+fi
