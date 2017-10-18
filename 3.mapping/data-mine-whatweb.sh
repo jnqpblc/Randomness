69fffaa4d1fbe8fc2a16611e3aa40b81f332bf5f
jnqpblc <jnqpblc@gmail.com>
2017-10-18 07:57:37 -0500

    Pushing changes...

---
+++ 3.mapping/data-mine-whatweb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z $1 ];then printf "\nSyntax: $0 <folder>\n\n"
+	else
+CLIENT=$1;
+cat -A $CLIENT/whatweb-output-*.txt|sed 's/[0-9]\{1,2\}m//g; s/\^//g; s/[\]]\{1,10\}/\]/g; s/[\[]\{1,10\}/\[/g; s/\[\]/\]/g; s/^ \[//g; s/\[,/,/g; s/\$//g;'|egrep -o '[0-9a-zA-Z]+\[[^]]+\]'|sort|uniq -c|sort -n > $CLIENT/counted.whatweb
+echo "Export created."
+ls -l $CLIENT/counted.whatweb;
+fi
+
