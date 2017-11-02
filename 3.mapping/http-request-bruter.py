9727d484b8b6db9fef8b4e601c940da3def98a4d
jnqpblc <jnqpblc@gmail.com>
2017-11-02 12:18:05 -0500

    Pushing changes...

--- 3.mapping/http-request-bruter.py
+++ 3.mapping/http-request-bruter.py
@@ -1,6 +1,6 @@
 import sys, socket
 if len(sys.argv) < 3:
-    sys.exit('\nUsage: %s <host|10.1.1.1> <port|443> <file|/tmp/burp.req> \n' % sys.argv[0])
+    sys.exit('\nUsage: %s <host|10.1.1.1> <port|443> <file|/tmp/burp.req>\n' % sys.argv[0])
 
 host = str(sys.argv[1])
 port = int(sys.argv[2])
